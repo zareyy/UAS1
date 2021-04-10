0cad1d116f42e23ed1babccf4b2b7965c96328c5
zareyy <79981953+zareyy@users.noreply.github.com>
2021-04-11 01:14:56 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$con = mysqli_connect("localhost","id16571603_user","hTc6O3w1XPDy#B\U","id16571603_uasweb");
+// Check connection
+if (mysqli_connect_error())
+{
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+?>
